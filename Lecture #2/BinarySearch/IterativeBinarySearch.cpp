2e16828b904aa664ec319dea5433c913a5e2c86a
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-08-29 12:09:44 +0600

    Lecture 2 #1

---
+++ Lecture #2/BinarySearch/IterativeBinarySearch.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    long long x;
+    cin>>n;
+    cin>>x;
+    vector<long long>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr.begin(),arr.end());
+    int l=0;
+    int r=n-1;
+    bool found=false;
+    long long ans=-1;
+    while(l<=r){
+        int mid=(l+r)/2;
+        if(arr[mid]==x){
+            found=true;
+            ans=mid;
+            break;
+        }
+        if(arr[mid]<x){
+            l=mid+1;
+        }
+        else{
+            r=mid-1;
+        }
+    }
+
+    if(!found){
+        cout<<"Not Found"<<endl;
+    }
+    else{
+        cout<<"Found at: "<<ans<<endl;
+    }
+    return 0;
+}
